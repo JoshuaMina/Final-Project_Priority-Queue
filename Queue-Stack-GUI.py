7d7a4e5e47afa237a165d7b8511e8ec0090e968f
JoshuaMina <joshua.minacambronero@gmail.com>
2023-02-17 18:04:54 +0800

    QueueStack GUI

--- Queue-Stack-GUI.py
+++ Queue-Stack-GUI.py
@@ -12,3 +12,10 @@ class Queue:
 
     def is_empty(self):
         return len(self.items) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[0]
+
+    def size(self):
+        return len(self.items)
