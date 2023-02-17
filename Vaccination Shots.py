dd574b01e1efefd8f4de95e3761dff17a5489af0
JoshuaMina <joshua.minacambronero@gmail.com>
2023-02-17 17:54:51 +0800

    Priority application

--- Vaccination Shots.py
+++ Vaccination Shots.py
@@ -47,30 +47,27 @@ pq = PriorityQueue()
 k = 0
 
 
-while True:
-    while (pq.size()<=5):
-        k += 1
-        try:
-            user = ""
-            name = input("Enter the name of patient: ").capitalize()
-            age = int(input("Enter his/her age: "))
-            status = input("Enter the his/her status: ")
-        except TypeError:
-            print("Invalid Input")
-        else:
-            special = input("Are you Senior Citizen (SC), Persons with Disability (PWD) and Frontliner workers (FW) otherwise type NO : ").upper()
-            if special == "FW":
-                prio = "1"
-            elif special == "SC":
-                prio = "2"
-            elif special == "PWD":
-                prio = "3"
-            else:
-                prio = "4"
-            user = Builder(dict(patientNo= k,name=name, age=age, status=status),prio)
-            pq.insert(user)
-            pq.show()
-            print(pq.size())
+while (pq.size()<=5):
+    k += 1
+    user = ""
+    name = input("Enter the name of patient: ").capitalize()
+    age = int(input("Enter his/her age: "))
+    special = input("Are you Senior Citizen (SC), Persons with Disability (PWD) and Frontliner workers (FW) otherwise type NO : ").upper()
+
+    if special == "FW":
+        prio = "1"
+    elif special == "SC":
+        prio = "2"
+    elif special == "PWD":
+        prio = "3"
+    else:
+        prio = "4"
+
+    user = Builder(dict(patientNo= k,name=name, age=age, ),prio)
+    pq.insert(user)
+    pq.show()
+    print(pq.size())
+    continue
 
 #
 #
